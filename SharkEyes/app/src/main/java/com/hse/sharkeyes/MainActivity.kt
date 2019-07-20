6983a7aafc944ecc30f4ae7908e9827deede9a28
brian griffey <briangriffey@brians-MacBook-Pro.local>
2019-07-20 12:46:45 -0500

    stuff

--- SharkEyes/app/src/main/java/com/hse/sharkeyes/MainActivity.kt
+++ SharkEyes/app/src/main/java/com/hse/sharkeyes/MainActivity.kt
@@ -2,11 +2,33 @@ package com.hse.sharkeyes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    override fun onInit(status: Int) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    lateinit var tts: TextToSpeech
+
+    protected fun speak(text: String) {
+        tts.speak("Stephen, you are one sexy human being", TextToSpeech.QUEUE_FLUSH, null, "id")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tts.shutdown()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tts = TextToSpeech(this, this)
+
+        button.setOnClickListener(View.OnClickListener { speak("") })
     }
 }
--- SharkEyes/app/src/main/java/com/hse/sharkeyes/ReadingActivity.kt
+++ SharkEyes/app/src/main/java/com/hse/sharkeyes/ReadingActivity.kt
@@ -6,13 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 class ReadingActivity: AppCompatActivity(), TextToSpeech.OnInitListener {
 
     override fun onInit(status: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     val tts = TextToSpeech(this, this)
 
     protected fun speak(text: String) {
-        tts.speak("Test test", TextToSpeech.QUEUE_FLUSH, null, "id")
+        tts.speak("Stephen, you are one sexy human being", TextToSpeech.QUEUE_FLUSH, null, "id")
     }
 
     override fun onDestroy() {
