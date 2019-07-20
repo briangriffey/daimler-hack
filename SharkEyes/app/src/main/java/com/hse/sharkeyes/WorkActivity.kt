251aa0000a3074d6f5b0ef38e8cbe0844f6ce413
brian griffey <briangriffey@brians-MacBook-Pro.local>
2019-07-20 12:38:37 -0500

    Adding a speaking activity

---
+++ SharkEyes/app/src/main/java/com/hse/sharkeyes/WorkActivity.kt
@@ -0,0 +1,11 @@
+package com.hse.sharkeyes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WorkActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
